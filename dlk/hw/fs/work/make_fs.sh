9a8b59ded57d001efabdc4114977f5f215d35eb5
tk26eng <kota_58@yahoo.co.jp>
2019-10-29 19:47:01 +0900

    Save current state

--- dlk/hw/fs/work/make_fs.sh
+++ dlk/hw/fs/work/make_fs.sh
@@ -2,8 +2,17 @@
 
 IMG_NAME=./test_fs.img
 
+losetup -D
 dd if=/dev/zero of=$IMG_NAME bs=1024 count=4194304 #4GB
+#losetup --show -f -P $IMG_NAME
 parted -s $IMG_NAME -- mklabel gpt \
-mkpart primary fat32 2048s 4096s \
-mkpart primary ext4 8192s 16384s \
-print
+#parted -s /dev/loop0 -- mklabel gpt \
+mkpart primary fat32 4096s 16383s \
+mkpart primary ext4 16384s -1s \
+mkpart primary 2048s 4095s \
+print \
+
+# kpartx will make /dev/loop0p[1,2,...]
+kpartx -a $IMG_NAME
+mkfs.vfat -v -c -F 32 /dev/loop0p1
+mkfs.ext4 /dev/loop0p2
