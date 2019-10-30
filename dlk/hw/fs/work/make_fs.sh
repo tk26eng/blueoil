2a9f0dac099fb0f2db5381d43aead6a44a241d24
tk26eng <kota_58@yahoo.co.jp>
2019-10-30 20:57:10 +0900

    Save current status

--- dlk/hw/fs/work/make_fs.sh
+++ dlk/hw/fs/work/make_fs.sh
@@ -2,17 +2,39 @@
 
 IMG_NAME=./test_fs.img
 
-losetup -D
-dd if=/dev/zero of=$IMG_NAME bs=1024 count=4194304 #4GB
-#losetup --show -f -P $IMG_NAME
-parted -s $IMG_NAME -- mklabel gpt \
-#parted -s /dev/loop0 -- mklabel gpt \
-mkpart primary fat32 4096s 16383s \
-mkpart primary ext4 16384s -1s \
-mkpart primary 2048s 4095s \
-print \
+dd if=/dev/zero of=$IMG_NAME bs=512 count=14843750 # 7600*1000*100 Byte (95% of 8GB)
+
 
 # kpartx will make /dev/loop0p[1,2,...]
-kpartx -a $IMG_NAME
-mkfs.vfat -v -c -F 32 /dev/loop0p1
-mkfs.ext4 /dev/loop0p2
+#kpartx -a $IMG_NAME
+#mkfs.vfat -v -c -F 32 /dev/loop0p1
+#mkfs.ext4 /dev/loop0p2
+
+echo -e \
+"n\n"\
+"p\n"\
+"3\n"\
+"\n"\
+"+1M\n"\
+"n\n"\
+"p\n"\
+"1\n"\
+"\n"\
+"+800M\n"\
+"n\n"\
+"p\n"\
+"2\n"\
+"\n"\
+"\n"\
+"t\n"\
+"1\n"\
+"c\n"\
+"t\n"\
+"2\n"\
+"83\n"\
+"t\n"\
+"3\n"\
+"a2\n"\
+"p\n"\
+"w\n"\
+ | fdisk $IMG_NAME
