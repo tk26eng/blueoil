44ddfc10e8cb70ca1b9fa7141b6fdf29fec5a59b
tk26eng <kota_58@yahoo.co.jp>
2019-11-15 15:34:41 +0900

    Commit current state

--- make_os/scripts/build_kernel.sh
+++ make_os/scripts/build_kernel.sh
@@ -5,24 +5,28 @@ SCRIPTS_DIR=/work/scripts
 LINUX_KERNEL_DIR=/work/linux_kernel
 DRIVER_DIR=/work/driver
 
-ROOTFS_DIR=${BUILD_DIR}/rootfs_${DEBOOTSTRAP_ARCH}
-ROOTFS_TGZ=${ROOTFS_DIR}.tgz
+#ROOTFS_DIR=${BUILD_DIR}/rootfs_${DEBOOTSTRAP_ARCH}
+#ROOTFS_TGZ=${ROOTFS_DIR}.tgz
+
+LINUX_DIR=${BUILD_DIR}/linux
+LINUX_TGZ=${LINUX_DIR}.tgz
 
 ######
 ### This is main part
 ######
+cd ${BUILD_DIR} # Just in case, we move to the directory 
 wget -i ${LINUX_KERNEL_DIR}/sources.txt -P ${BUILD_DIR}
-cd ${BUILD_DIR}
-unzip socfpga-4.5.zip
-cd ${BUILD_DIR}/linux-socfpga-socfpga-4.5
-cp ${LINUX_KERNEL_DIR}/config ./.config
+unzip socfpga-4.5.zip -d ${BUILD_DIR}
+mv ${BUILD_DIR}/linux-socfpga-socfpga-4.5 ${LINUX_DIR}
+cp ${LINUX_KERNEL_DIR}/config ${LINUX_DIR}/.config
+
+cd ${LINUX_DIR}
 ln -s /usr/bin/arm-linux-gnueabihf-gcc-7 /usr/local/bin/arm-linux-gnueabihf-gcc
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
-make # If use -j option, it'll be faster
-
-mkdir ${BUILD_DIR}/modules
-make modules_install INSTALL_MOD_PATH=${BUILD_DIR}/modules
+make -j 32 # If we use -j option, it'll be faster than single core
+mv ${LINUX_DIR}/arch/arm/boot/zImage ${BUILD_DIR}
+make modules_install INSTALL_MOD_PATH=${BUILD_DIR}
 
 # Tar part is necessary later. It should be holded.
 #rm ${ROOTFS_DIR}/setting_after_chroot.sh
@@ -33,3 +37,8 @@ make modules_install INSTALL_MOD_PATH=${BUILD_DIR}/modules
 
 #tar cvzf ${ROOTFS_TGZ} -C `dirname ${ROOTFS_DIR}` `basename ${ROOTFS_DIR}` --remove-file
 #chmod a=rw ${ROOTFS_TGZ}
+
+
+# These two lines  will be used on the anather script
+#mkdir ${BUILD_DIR}/modules
+
